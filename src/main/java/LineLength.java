6165d22f4c58c300cdd377d901dd713a0b96ac29
Curtis Rueden <ctrueden@wisc.edu>
2010-09-14 21:35:57 +0000

    Fix up license for utils project.

--- src/main/java/LineLength.java
+++ src/main/java/LineLength.java
@@ -2,6 +2,36 @@
 // LineLength.java
 //
 
+/*
+A collection of simple Java utilities.
+
+Copyright (c) 2006, UW-Madison LOCI
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the UW-Madison LOCI nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
@@ -15,8 +45,8 @@ import java.util.Arrays;
  * Also checks for tabs and end-of-line spaces.
  *
  * <dl><dt><b>Source code:</b></dt>
- * <dd><a href="https://skyking.microscopy.wisc.edu/trac/java/browser/trunk/utils/LineLength.java">Trac</a>,
- * <a href="https://skyking.microscopy.wisc.edu/svn/java/trunk/utils/LineLength.java">SVN</a></dd></dl>
+ * <dd><a href="http://dev.loci.wisc.edu/trac/java/browser/trunk/projects/utils/src/main/java/LineLength.java">Trac</a>,
+ * <a href="http://dev.loci.wisc.edu/svn/java/trunk/projects/utils/src/main/java/LineLength.java">SVN</a></dd></dl>
  */
 public class LineLength implements FileFilter {
 
@@ -24,8 +54,8 @@ public class LineLength implements FileFilter {
   private static final int TABWIDTH = 2;
 
   private static final String[] LOCI_HACKS = {
-    " * <dd><a href=\"https://skyking.microscopy.wisc.edu/trac/",
-    " * <a href=\"https://skyking.microscopy.wisc.edu/svn/java/trunk/"
+    " * <dd><a href=\"http://dev.loci.wisc.edu/trac/",
+    " * <a href=\"http://dev.loci.wisc.edu/svn/java/trunk/"
   };
 
   public static void main(String[] args) throws IOException {
